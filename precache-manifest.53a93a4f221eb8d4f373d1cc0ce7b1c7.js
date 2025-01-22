fef43fd12d8be4ffd7475d3677bdbb23bbef5c72
itspranavnow <pranav4139@gmail.com>
2025-01-22 20:16:21 +0530

    Updates

--- precache-manifest.53a93a4f221eb8d4f373d1cc0ce7b1c7.js
+++ precache-manifest.53a93a4f221eb8d4f373d1cc0ce7b1c7.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a90a8e2ac194beef8ea4e4393f81a421",
+    "revision": "c3b2475815e7c44f516b6327d087044a",
     "url": "/my_portfolio/index.html"
   },
   {
-    "revision": "29670f8a98608d56a377",
+    "revision": "ebf75041cd819979a6f7",
     "url": "/my_portfolio/static/css/main.b87d8b28.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my_portfolio/static/js/2.810a3ca9.chunk.js"
   },
   {
-    "revision": "29670f8a98608d56a377",
-    "url": "/my_portfolio/static/js/main.e8ed8210.chunk.js"
+    "revision": "ebf75041cd819979a6f7",
+    "url": "/my_portfolio/static/js/main.1d8af99e.chunk.js"
   },
   {
     "revision": "f4e0835a9f15902d0882",

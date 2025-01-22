7053f97d1c9cdede2bf29070c21120c6d6b2ddd9
itspranavnow <pranav4139@gmail.com>
2025-01-22 20:47:07 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my_portfolio/precache-manifest.9a7892bea84b966bed05a623199c280a.js"
+  "/my_portfolio/precache-manifest.6107f8593c459277d38dce11a92f9e7c.js"
 );
 
 self.addEventListener('message', (event) => {
